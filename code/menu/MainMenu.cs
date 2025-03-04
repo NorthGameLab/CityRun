c2a67b37b9a07d1eb992af188f0cb36538f29062
Aatkem <aatu.kemppainen@tuni.fi>
2025-03-04 14:08:25 +0200

    päivitetyt hahmot

--- code/menu/MainMenu.cs
+++ code/menu/MainMenu.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class MainMenu : Node
 {
+    private AnimatedSprite2D _animation = null;
     public CanvasLayer Menu;
     public Label HighScore;
 
@@ -12,6 +13,15 @@ public partial class MainMenu : Node
         HighScore = Menu.GetNode<Label>("HighScore");
 
         HighScore.Text = "Highscore: " + Test.HighScore;
+
+        _animation = GetNode<AnimatedSprite2D>("Menu/Control/AnimatedSprite2D");
+        _animation.Play();
+        _animation.SpeedScale = 1;
+
+
+    }
+    public override void _Process(double delta)
+    {
 
     }
     private void onStartButtonPressed()
