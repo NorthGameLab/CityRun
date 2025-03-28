94418bfae0e59b0848353f346f414fbffeab7f20
Eemil Akerlund <eemil.akerlun@outlook.com>
2025-03-28 14:30:24 +0200

    commit 22

--- code/item/Coin.cs
+++ code/item/Coin.cs
@@ -11,15 +11,13 @@ public partial class Coin : Item
 		if (body is Player player)
 		{
 			Test.addToMoney(1);
-            test = testA.Instantiate<TestA>();
-            //test.Position = new Vector2(110, 15);
-            GetParent().AddChild(test);
-            test.GetNode<Sprite2D>("Sprite2D").Show();
+      test = testA.Instantiate<TestA>();
+      //test.Position = new Vector2(110, 15);
+      GetParent().AddChild(test);
+      test.GetNode<Sprite2D>("Sprite2D").Show();
 
-            GameScene gameScene = GetTree().Root.GetNode<GameScene>("GameScene");
-            gameScene.PlaySound(GD.Load<AudioStream>("res://Audio/CoinCollect2.mp3"));
-
-
+      GameScene gameScene = GetTree().Root.GetNode<GameScene>("GameScene");
+      gameScene.PlaySound(GD.Load<AudioStream>("res://Audio/CoinCollect2.mp3"));
 		}
-    }
+  }
 }
